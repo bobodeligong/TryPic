d30b58e1d674c6c1d02a564b4615818926f00c5a
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2021-02-17 02:19:14 -0800

    Add files via upload

---
+++ task_main.js
@@ -0,0 +1,9 @@
+var repo_site ="https://bobodeligong.github.io/TryPic/";
+var timeline =[];
+var TryPic = {
+    type:"image-keyboard-response",
+    stimulus: repo_site + "emotion1.png",
+    choices: ['1','2','3','4'],
+    prompt: "<p>Emotion rating from 1 to 4</p>",
+};
+timeline.push(TryPic);
